23760442a623d88a16afdbf0ed651d7c0bb3dae0
Viharika <viharika807@gmail.com>
2023-05-19 10:37:48 +0530

    step-4

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.set('view engine', 'ejs');
+
+const mappings = {}; 
 
 const endpoints = [
   { path: '/', description: 'Home page', response: 'Rendered HTML' },
   { path: '/i/google', description: 'Link to Google', response: 'HTML anchor tag' },
   { path: '/i/github', description: 'Link to Github', response: 'HTML anchor tag' },
+  { path: '/i', description: 'List all the endpoints that are available', response: 'Endpoints List' },
   { path: '/r/google', description: 'Redirect to Google', response: 'Redirect to https://google.com' },
   { path: '/r/github', description: 'Redirect to Github', response: 'Redirect to https://github.com' },
   { path: '/r', description: 'Information about redirection endpoint', response: 'Message about how to use /r/google and /r/github' },
+  { path: '/map', description: 'Save URL to alias mapping', response: 'Save a mapping between a URL and an alias' },
+  { path: '/mappings', description: 'Display alias-to-URL mappings', response: 'List of alias-to-URL mappings' },
 ];
 
 app.get('/', (req, res) => {
@@ -30,7 +37,7 @@ app.get('/i/github', (req, res) => {
 })
 
 app.get('/r', (req, res) => {
-  const message = 'To use the redirection endpoint, make a GET request to /r/google.';
+  const message = 'To use the redirection endpoint, make a GET request to /r/google and /r/github.';
   res.send(message);
 });
 
@@ -41,6 +48,19 @@ app.get('/r/google', (req, res) => {
 app.get('/r/github', (req, res) => {
   res.redirect('https://github.com/');
 })
+
+app.post('/map', (req, res) => {
+  const { url, alias } = req.body;
+  if (!url || !alias) {
+    return res.status(400).send('Both URL and alias are required.');
+  }
+  mappings[alias] = url;
+  res.send('Mapping saved successfully.');
+});
+
+app.get('/mappings', (req, res) => {
+  res.json(mappings);
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server is running at port 5000');
