08796a712dddd631b0df9db17574d42650a068b8
Viharika <viharika807@gmail.com>
2023-05-19 10:13:06 +0530

    step-3

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const endpoints = [
   { path: '/', description: 'Home page', response: 'Rendered HTML' },
   { path: '/i/google', description: 'Link to Google', response: 'HTML anchor tag' },
   { path: '/i/github', description: 'Link to Github', response: 'HTML anchor tag' },
+  { path: '/r/google', description: 'Redirect to Google', response: 'Redirect to https://google.com' },
+  { path: '/r/github', description: 'Redirect to Github', response: 'Redirect to https://github.com' },
+  { path: '/r', description: 'Information about redirection endpoint', response: 'Message about how to use /r/google and /r/github' },
 ];
 
 app.get('/', (req, res) => {
@@ -24,6 +27,19 @@ app.get('/i', (req, res) => {
 
 app.get('/i/github', (req, res) => {
   res.send('<a href="https://github.com/">Go to Github</a>');
+})
+
+app.get('/r', (req, res) => {
+  const message = 'To use the redirection endpoint, make a GET request to /r/google.';
+  res.send(message);
+});
+
+app.get('/r/google', (req, res) => {
+  res.redirect('https://www.google.com');
+})
+
+app.get('/r/github', (req, res) => {
+  res.redirect('https://github.com/');
 })
 
 app.listen(process.env.PORT || 5000, () => {
